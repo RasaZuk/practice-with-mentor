7ab03c5fdafd75aa2ba9ac002c0df4c63878732e
Rasa <rasa.zukauskaite@gmail.com>
2024-08-08 12:01:10 +0300

    new project

---
+++ country/country.js
@@ -0,0 +1,35 @@
+const searchInput = document.getElementById('search');
+const enter = document.querySelector('.enter');
+const btn = document.querySelector('.btn');
+const list = document.querySelector('#list');
+
+btn.addEventListener('click', () => {
+    const value = enter.value
+    if (value.trim() === "") {
+        alert('Įveskite šalį')
+        return
+    }
+    else {
+        list.append(Object.assign(document.createElement('li'), { textContent: value }))
+    }
+
+})
+
+search.addEventListener("keyup", search)
+
+function search() {
+    const searchInputValue = searchInput.value
+
+    const listElements = document.getElementsByTagName("li")
+
+    for (let i = 0; i < listElements.length; i++) {
+        if (listElements[i].innerHTML.toLocaleLowerCase().includes(searchInputValue) || listElements[i].innerHTML.toUpperCase().includes(searchInputValue)) {
+            listElements[i].style.display = ""
+        }
+        else {
+            listElements[i].style.display = "none"
+        }
+    }
+
+
+}
